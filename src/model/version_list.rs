0ce304822541e4f3be9134192ce1822da99e4f3a
Jason Franklin <jason.franklin@xing.com>
2019-11-26 17:23:59 +0100

    Reverse version list so that newest updated_at is first

--- src/model/version_list.rs
+++ src/model/version_list.rs
@@ -5,6 +5,7 @@ impl VersionList {
     pub fn latest_version(versions: Vec<Version>) -> Option<Version> {
         let mut sorted_versions = versions.to_vec();
         sorted_versions.sort_by(|a, b| a.uploaded_at.cmp(&b.uploaded_at));
+        sorted_versions.reverse();
         sorted_versions.first().map(|v| v.clone())
     }
 }
