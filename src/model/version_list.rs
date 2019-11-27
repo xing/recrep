2290aa76b3f8360e93eea0f7c5d78721edb13153
Jason Franklin <jason.franklin@xing.com>
2019-11-27 11:34:39 +0100

    Add test case for sorted version list

--- src/model/version_list.rs
+++ src/model/version_list.rs
@@ -8,3 +8,26 @@ impl VersionList {
         sorted_versions.first().map(|v| v.clone())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_sorting_of_version_list() {
+        let version1 = Version { short_version: String::from("1.0"), uploaded_at: String::from("2019-11-16T22:29:48.000Z") };
+        let version2 = Version { short_version: String::from("1.0"), uploaded_at: String::from("2019-11-17T22:29:48.000Z") };
+        let version3 = Version { short_version: String::from("1.0"), uploaded_at: String::from("2019-11-18T22:29:48.000Z") };
+        let mut vec = Vec::new();
+
+        vec.push(version1);
+        vec.push(version2);
+        vec.push(version3);
+
+        let latest = VersionList::latest_version(vec);
+        match latest {
+            Some(latest) => assert_eq!(latest.uploaded_at, String::from("2019-11-18T22:29:48.000Z")),
+            None => panic!("There was no latest version in the returned sorted list")
+        }
+    }
+}
