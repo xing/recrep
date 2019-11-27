942d2a5b692c9725d52bc954225bdb5cfb2bc9b1
Jason Franklin <jason.franklin@xing.com>
2019-11-27 10:12:01 +0100

    CR: Change the sorting instead of using extra reverse operation

--- src/model/version_list.rs
+++ src/model/version_list.rs
@@ -4,8 +4,7 @@ pub struct VersionList {}
 impl VersionList {
     pub fn latest_version(versions: Vec<Version>) -> Option<Version> {
         let mut sorted_versions = versions.to_vec();
-        sorted_versions.sort_by(|a, b| a.uploaded_at.cmp(&b.uploaded_at));
-        sorted_versions.reverse();
+        sorted_versions.sort_by(|a, b| b.uploaded_at.cmp(&a.uploaded_at));
         sorted_versions.first().map(|v| v.clone())
     }
 }
