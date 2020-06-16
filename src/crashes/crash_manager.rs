1cc04036c92cb696c873a5adfe74473533d0b08e
Michael Jäger <Michael.Jaeger@xing.com>
2020-06-16 16:11:32 +0200

    [crash_manager.rs] assign directly via pattern matching

--- src/crashes/crash_manager.rs
+++ src/crashes/crash_manager.rs
@@ -75,10 +75,9 @@ impl CrashManager {
             .latest_version(organization.to_string(), application.to_string())
             .expect("Missing version json.");
         let versions = VersionListParser::versions(&latest_version_json).unwrap();
-        let latest_version;
-        match distribution_group {
-            Some(group) => latest_version = VersionList::latest_version_of_distribution_group(versions, group),
-            None        => latest_version = VersionList::latest_version(versions)
+        let latest_version = match distribution_group {
+            Some(group) => VersionList::latest_version_of_distribution_group(versions, group),
+            None        => VersionList::latest_version(versions)
         };
 
         match latest_version {
