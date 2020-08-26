d419983b0ebb6065e734ee7dae976804ee7a0ab7
Michael Jäger <michael.jaeger@xing.com>
2020-08-26 18:09:11 +0200

    [lib.rs] Add unit test for formatting a report including threshold

--- src/lib.rs
+++ src/lib.rs
@@ -194,3 +194,13 @@ This report was created using `recrep` for {{organization}}/{{application}}/{{ve
         )
     }
 }
+
+#[test]
+//Formats a crash report including a threshold value
+fn test_report_formatting_supports_threshold() {
+
+    let reporter = CrashReporter::with_token("abc", "org name", "app id", None, None, Some(300));
+    let report = utils::test_helper::TestHelper::report_from_json("src/json_parsing/test_fixtures/two_crashes.json");
+    let formatted_report = reporter.format_report(report);
+    assert_eq!(formatted_report.chars().count(), 1172)
+}
